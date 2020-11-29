6902e206a70a7afd2b2ebc793de9c6e4ea9a2946
Rushanthan <rushan26@live.com>
2020-11-29 15:18:46 +0530

    Login page first commit, tilt image, login form fields

--- wp-content/themes/student/footer.php
+++ wp-content/themes/student/footer.php
@@ -1,6 +1,14 @@
 	<!-- Bootstrap core JavaScript -->
 	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/jquery.slim.min.js"></script>
 	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/bootstrap.bundle.min.js"></script>
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/tilt/tilt.jquery.min.js"></script>
+	
+	<script>
+		$('.logimg-tilt').tilt({
+			glare: true,
+    		maxGlare: .5
+		})
+	</script>
 	<?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/student/index.php
+++ wp-content/themes/student/index.php
@@ -1,12 +1,39 @@
 <?php get_header(); ?>
 	<!-- Page Content -->
-	<div class="container">
+	<div class="page-limit">
+	<div class="login-container">
+		<div class="login-wrap">
+			<div class="container">
 		<div class="row">
-			<div class="col-lg-12 text-center">
+			<div class="col-lg-6">
+				<div class="page-title">
+					<div class="logimg-tilt" data-tilt>
+						<img src="<?php echo get_bloginfo('template_directory'); ?>/img/login-form-img.jpg" class="img-fluid">
+</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
 				<div class="page-title">
 					<h1>Student Registration</h1>
+					<form action="/action_page.php">
+						<div class="form-group">
+						<label for="email">Email:</label>
+						<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+						</div>
+						<div class="form-group">
+						<label for="pwd">Password:</label>
+						<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+						</div>
+						<div class="checkbox">
+						<label><input type="checkbox" name="remember"> Remember me</label>
+						</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form>
 				</div>
 			</div>
 		</div>
+</div>
+</div>
 	</div>
+</div>
 <?php get_footer(); ?>
